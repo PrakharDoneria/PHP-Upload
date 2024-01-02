ac58c73b42beb8cdb286be78f12925238cc89a41
Prakhar Doneria <gamesprotec@gmail.com>
2024-01-02 14:28:31 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,16 +5,19 @@ $uploadOk = 1;
 // Check if the file was sent without errors
 if ($_FILES["fileToUpload"]["error"] == 0) {
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $uploadedFileName = basename($_FILES["fileToUpload"]["name"]);
 
     // Check if file already exists
     if (file_exists($targetFile)) {
-        echo "Sorry, file already exists.";
+        $errorMsg = "Sorry, file already exists.";
+        echo "<script>openModal('$errorMsg');</script>";
         $uploadOk = 0;
     }
 
     // Check file size (adjust as needed)
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        $errorMsg = "Sorry, your file is too large.";
+        echo "<script>openModal('$errorMsg');</script>";
         $uploadOk = 0;
     }
 
@@ -23,22 +26,28 @@ if ($_FILES["fileToUpload"]["error"] == 0) {
     $uploadedFileExtension = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
     if (!in_array($uploadedFileExtension, $allowedExtensions)) {
-        echo "Sorry, only HTML files are allowed.";
+        $errorMsg = "Sorry, only HTML files are allowed.";
+        echo "<script>openModal('$errorMsg');</script>";
         $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        $errorMsg = "Sorry, your file was not uploaded.";
+        echo "<script>openModal('$errorMsg');</script>";
     } else {
         // Upload the file
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            $successMsg = "The file $uploadedFileName has been uploaded.";
+            $websiteLink = "https://protec-web-services.42web.io/$uploadedFileName";
+            echo "<script>openModal('$successMsg'); window.location.replace('$websiteLink');</script>";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            $errorMsg = "Sorry, there was an error uploading your file.";
+            echo "<script>openModal('$errorMsg');</script>";
         }
     }
 } else {
-    echo "Error during file upload.";
+    $errorMsg = "Error during file upload.";
+    echo "<script>openModal('$errorMsg');</script>";
 }
 ?>
